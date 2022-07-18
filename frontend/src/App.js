1076b2d95baca19d178272515003f91edf04dee0
Sahil Rajput <sahilrajput03@gmail.com>
2022-07-18 21:52:41 +0530

    BUILD DEPLOY!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -174,10 +174,10 @@ const Room = (props) => {
 				//
 				video: {width: 320}, // and height will be 240 according to 4:3 ration.
 				// video: true,
-				// audio: {
-				// 	sampleSize: 8,
-				// 	echoCancellation: true,
-				// },
+				audio: {
+					sampleSize: 8,
+					// echoCancellation: true, // read above comments coz echoCancellatin is diabled for safari most probably.
+				},
 			})
 			.then((stream) => {
 				_stream = stream // bcoz we would need to close webcam and mic access manually on disconnect button event or navigating to home component directy from the Room component.
